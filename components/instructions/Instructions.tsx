60b544d0a776d365b311917a3a3f224037b55cfe
rjatin007 <rjatin1991@gmail.com>
2025-02-18 00:00:16 +0530

    Update Instructions

--- components/instructions/Instructions.tsx
+++ components/instructions/Instructions.tsx
@@ -6,7 +6,10 @@ function Instructions() {
       <h2 className="text-lg font-semibold my-2">How to Attempt?</h2>
       {Object.keys(instructions).map((k) => (
         <p key={`${instructions[k].slice(1, 3)}-${k}`} className="text-md my-2">
-          Write <span className="font-semibold">{k}</span> {instructions[k]}{" "}
+          Write <span className="font-semibold">{k}</span> implying{" "}
+          <span className="font-semibold">
+            {instructions[k].toLowerCase()}{" "}
+          </span>{" "}
         </p>
       ))}
     </div>
